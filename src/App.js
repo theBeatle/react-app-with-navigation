977d3040fa8eb9371aadd28b18093a80db3a9b32
DESKTOP-EPN9CP6\thebe <oleh.knyaz@gmail.com>
2019-04-04 19:30:32 +0300

    added componets for pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import 'bulma/css/bulma.css'
+
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+
+import Home from './Home';
+import Weather from './WeatherApp';
+import StarWars from './StarWarsApp';
+import TodoList from './TodoListApp';
+import About from './About';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+        <div className="App">
+          {/* <h1>Hello from multi page App on React.JS</h1> */}
+          
+          {/* define block with routes */}
+          <ul className="header">
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/weather">Weather App</NavLink></li>
+            <li><NavLink to="/star-wars">Star Wars API App</NavLink></li>
+            <li><NavLink to="/todo-list">Todo List App</NavLink></li>
+            <li><NavLink to="/about">About</NavLink></li>
+          </ul>
+
+          {/* define block with exact workers for predefined routes */}
+          <div className="container">
+            <Route exact path="/" component={Home} />
+            <Route path="/weather" component={Weather} />
+            <Route path="/star-wars" component={StarWars} />
+            <Route path="/todo-list" component={TodoList} />
+            <Route path="/about" component={About} />
+          </div>
+        </div>
+      </HashRouter>
+
     );
   }
 }
